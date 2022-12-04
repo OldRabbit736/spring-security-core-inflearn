ddede0dc85544a73bd0ae15ff5cc291a79c4e61e
OldRabbit736 <sylvan0212@gmail.com>
2022-12-04 12:41:02 +0900

    진행 중: 실전프로젝트 - 인가 프로세스 DB 연동 웹 계층 구현 | 2) 관리자 시스템 - 권한 도메인, 서비스, 리포지토리 구성

--- follow-along/core-spring-security/src/main/java/com/example/corespringsecurity/controller/admin/ResourcesController.java
+++ follow-along/core-spring-security/src/main/java/com/example/corespringsecurity/controller/admin/ResourcesController.java
@@ -6,14 +6,15 @@ import com.example.corespringsecurity.domain.entity.Role;
 import com.example.corespringsecurity.repository.RoleRepository;
 import com.example.corespringsecurity.service.ResourcesService;
 import com.example.corespringsecurity.service.RoleService;
-import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 public class ResourcesController {
@@ -34,6 +35,18 @@ public class ResourcesController {
         List<Resources> resources = resourcesService.getResources();
         model.addAttribute("resources", resources);
         return "admin/resource/list";
+    }
+
+    @GetMapping("/admin/resources/register")
+    public String viewRoles(Model model) {
+        List<Role> roleList = roleService.getRoles();
+        model.addAttribute("roleList", roleList);
+        ResourcesDto resources = new ResourcesDto();
+        Set<Role> roleSet = new HashSet<>();
+        roleSet.add(new Role());
+        resources.setRoleSet(roleSet);
+        model.addAttribute("resources", resources);
+        return "admin/resource/detail";
     }
 
     @GetMapping("/admin/resources/{id}")
