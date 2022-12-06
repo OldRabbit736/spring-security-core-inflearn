a53e2a3a7f87fb2041b39de7557fd330057b90bb
OldRabbit736 <sylvan0212@gmail.com>
2022-12-06 15:06:36 +0900

    완료: 실전프로젝트 - 인가 프로세스 DB 연동 웹 계층 구현 | 7) 인가처리 허용 필터 - PermitAllFilter 구현

--- follow-along/core-spring-security/src/main/java/com/example/corespringsecurity/security/config/SecurityConfig.java
+++ follow-along/core-spring-security/src/main/java/com/example/corespringsecurity/security/config/SecurityConfig.java
@@ -129,7 +129,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
-    @Bean
+    /**
+     * web.ignoring 세팅된 CSS, IMAGES 리소스 파일도 PermitAllFilter 에 의해 핸들링되는 현상이 발생하였다.
+     * 분명 CSS, IMAGES 파일 요구 request가 빈(empty) SecurityFilterChain 에 매칭되는 것을
+     * FilterChainProxy::getFilters(HttpServletRequest request)에서 확인하였다.
+     * 그러나 결국 PermitAllFilter가 request에 적용되어 버리는 것이었다. 왜??
+     * 구글링 해보니 Filter를 Bean 등록하게 되면 SecurityFilter 외에도 다른 곳에 등록되어, Filter가 원래 등록되었던 SecurityFilterChain
+     * 을 피하더라도 결국 해당 Filter가 적용된다는 것이었다.
+     * https://stackoverflow.com/questions/39152803/spring-websecurity-ignoring-doesnt-ignore-custom-filter
+     * 정확한 원인은 여기서도 알 수는 없지만 대략적인 대처 방안은 알 수 있었다.
+     * 위 페이지에서 제시한 대로 PermitAllFilter 를 @Bean 어노테이션으로 등록한 부분을 삭제하였더니 문제가 해결되었다.
+     * 즉 web.ignoring 세팅된 리소스 파일이 더 이상 PermitAllFilter 에 걸리지 않게 되었다.
+     */
+    //@Bean
     public PermitAllFilter customFilterSecurityInterceptor() throws Exception {
         PermitAllFilter permitAllFilter = new PermitAllFilter(permitAllResources);
         permitAllFilter.setSecurityMetadataSource(urlFilterInvocationSecurityMetadataSource());
@@ -148,7 +160,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public FilterInvocationSecurityMetadataSource urlFilterInvocationSecurityMetadataSource() {
+    public UrlFilterInvocationSecurityMetadataSource urlFilterInvocationSecurityMetadataSource() {
         return new UrlFilterInvocationSecurityMetadataSource(urlResourcesMapFactoryBean().getObject(), securityResourceService);
     }
 
