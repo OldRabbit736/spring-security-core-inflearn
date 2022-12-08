9777b289ede9715b1d84198cadd6959129447f12
OldRabbit736 <sylvan0212@gmail.com>
2022-12-08 22:20:42 +0900

    완료: 실전프로젝트 - 인가 프로세스 DB 연동 서비스 계층 구현 | 2) AOP Method 기반 DB 연동 - 주요 아키텍쳐 이해

--- follow-along/core-spring-security/src/main/java/com/example/corespringsecurity/controller/user/UserController.java
+++ follow-along/core-spring-security/src/main/java/com/example/corespringsecurity/controller/user/UserController.java
@@ -5,10 +5,14 @@ import com.example.corespringsecurity.domain.dto.AccountDto;
 import com.example.corespringsecurity.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,11 +20,6 @@ public class UserController {
 
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
-
-    @GetMapping("/mypage")
-    public String page() {
-        return "user/mypage";
-    }
 
     @GetMapping("/users")
     public String createUser() {
@@ -37,4 +36,9 @@ public class UserController {
         return "redirect:/";
     }
 
+    @GetMapping("/mypage")
+    public String page(@AuthenticationPrincipal Account account, Authentication authentication, Principal principal) {
+        userService.order();
+        return "user/mypage";
+    }
 }
